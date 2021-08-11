433b0a3111b5fe5325390ec58341df0e585ba46d
douguizilla <douglasgommez@gmail.com>
2021-08-11 12:51:10 -0300

    MainActivity.kt message was refactored and add strings.xml

--- app/src/main/java/com/odougle/basic/MainActivity.kt
+++ app/src/main/java/com/odougle/basic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.odougle.basic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,15 +8,23 @@ import com.odougle.basic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var message: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        message = binding.edtText.text.toString()
+
         binding.btnToast.setOnClickListener{
-            var message: String? = binding.edtText.text.toString()
             message?.let {
                 createToast(it)
             }
+        }
+
+        binding.btnNextActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("TEXT_FROM_ACTIVITY", message)
+            startActivity(intent)
         }
     }
 
