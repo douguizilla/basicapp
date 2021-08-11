8bb776ed7c748e55fa5464a1534dd56c164bed54
douguizilla <douglasgommez@gmail.com>
2021-08-11 12:37:33 -0300

    set view binding

--- app/src/main/java/com/odougle/basic/SecondActivity.kt
+++ app/src/main/java/com/odougle/basic/SecondActivity.kt
@@ -2,10 +2,12 @@ package com.odougle.basic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.basic.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private val binding: ActivitySecondBinding by lazy { ActivitySecondBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(binding.root)
     }
 }
